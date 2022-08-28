86c078669a68fec9dd66e2188ea7604f63e2726e
AngelPlayer <ssh5212@naver.com>
2022-08-28 20:25:48 +0900

    commit

---
+++ Raspberry Pi/total.py
@@ -0,0 +1,24 @@
+#-*-coding:utf-8-*-
+
+# Button Code [S +] --------------------------------------- 
+# 필요한 라이브러리를 불러옵니다.
+import RPi.GPIO as GPIO 
+import time
+
+
+# button_callback function
+def button_callback(channel):
+    pass
+
+
+button_pin = 15 # 사용할 GPIO핀의 번호 설정
+GPIO.setwarnings(False) 
+GPIO.setmode(GPIO.BCM) 
+GPIO.setup(button_pin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN) # 버튼 핀의 IN/OUT 설정 , PULL DOWN 설정
+ 
+GPIO.add_event_detect(button_pin,GPIO.RISING,callback=button_callback) # 신호 감지 시 button_callback 함수 실행
+
+while 1:
+     time.sleep(0.1) # 0.1초 딜레이
+# Button Code [E] --------------------------------------- 
+     
